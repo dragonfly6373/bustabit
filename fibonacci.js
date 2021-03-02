f61e3940db86548176db41406d7cd8760a918346
dragonfly6373 <nguyendang5389@gmail.com>
2021-03-02 21:59:11 +0700

    fibonacci - fix bug

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
 var config = {
     baseBet: { value: 100, type: "balance", label: "Base Bet:"},
-    skippedPayout: { value: 1.6, type: "multiplier", label: "Min Payout Skipped"}
+    skippedPayout: { value: 2, type: "multiplier", label: "Min Payout Skipped"}
 };
 
 function getBit(satoshis) {
@@ -17,20 +17,20 @@ function Fibonacci(baseBet, skippedPayout) {
     this.skippedPayout = skippedPayout;
 
     this.wager = 1;
+    this.prevWager = 0;
     this.payout = 55;
     this.nextPayout = 34;
     this.lost = 0;
-    this.nextLost = 1;
 
     this.bustLogs = [];
 }
 
 Fibonacci.prototype.reset = function() {
     this.wager = 1;
+    this.prevWager = 0;
     this.payout = 55;
     this.nextPayout = 34;
     this.lost = 0;
-    this.nextLost = 1;
 }
 
 Fibonacci.prototype.saveLog = function(value) {
@@ -41,14 +41,19 @@ Fibonacci.prototype.saveLog = function(value) {
     }
 }
 
+Fibonacci.prototype.showLogs = function() {
+    log(this.bustLogs);
+    this.bustLogs = [];
+}
+
 Fibonacci.prototype.makeBet = function(engine) {
     if (this.skipped) {
         return;
     }
     var betAmount = this.wager * this.baseBet;
-    engine.bet(betAmount, this.payout);
     var message = "(" + this.bustLogs.length + ") makeBet(" + getBit(betAmount) + ", " + this.payout + ")";
-    log(message);
+    log(message, engine.cashOut());
+    engine.bet(betAmount, this.payout);
 }
 
 // data: {id: int, hash: string, bust: float, cashedAt: float, wager: double}
@@ -78,13 +83,12 @@ Fibonacci.prototype.onWin = function(data) {
 
 Fibonacci.prototype.onLose = function(data) {
     var amount = this.baseBet * this.wager;
-    this.nextLost += (this.lost > 0 ? this.lost : 1);
-    this.lost += this.wager;
-    this.wager = this.nextLost - this.lost;
-    log("[LOST]", getBit(amount), "bits", this.nextLost, this.lost, this.wager);
+    log("[LOST]", getBit(amount), "bits");
     var p = this.payout;
     this.payout = Math.max(2, this.nextPayout);
     this.nextPayout = p - this.payout;
+    this.lost += this.wager;
+    this.wager += this.payout > 2 ? (this.prevWager = this.wager) : this.lost;
 }
 
 var fibo = new Fibonacci(config.baseBet.value, config.skippedPayout.value);
@@ -97,6 +101,7 @@ if (engine.gameState === "GAME_STARTING") {
 
 engine.on("GAME_STARTING", onGameStarted);
 engine.once("GAME_STARTING", () => engine.on("GAME_ENDED", onGameEnded));
+engine.on("CASHED_OUT", fibo.showLogs);
 
 function onGameStarted() {
     if (!fibo) fibo = new Fibonacci(config.baseBet.value, config.skippedPayout.value);
